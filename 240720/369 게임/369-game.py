be2392fd71eaa1727f81e05da5e19fae066513db
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-20 21:52:39 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240720/369 게임/369-game.py
+++ 240720/369 게임/369-game.py
@@ -8,5 +8,6 @@ for i in range(1, n+1):
         for j in range(len(str(i))):
             if str(i)[j] == "3" or str(i)[j] == "6" or str(i)[j] == "9":
                 print(0, end=" ")
+                break
             else:
                 print(i, end=" ")
