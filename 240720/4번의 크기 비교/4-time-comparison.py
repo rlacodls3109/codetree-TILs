88f38cf510227aa6a416d6945d694145fcb01c74
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-20 17:08:15 +0900

    Title: 4번의 크기 비교, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+k = input()
+arr = k.split()
+
+b, c, d, e = int(arr[0]), int(arr[1]), int(arr[2]), int(arr[3])
+
+print(int(a>b))
+print(int(a>c))
+print(int(a>d))
+print(int(a>e))
