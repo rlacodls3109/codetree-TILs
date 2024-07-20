6f1d04e91c477a874b84fc07389f4f4b9d32d3d7
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-20 17:04:18 +0900

    Title: 4가지 관계연산자, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/4가지 관계연산자/four-relational-operators.py
+++ 240720/4가지 관계연산자/four-relational-operators.py
@@ -1,9 +1,9 @@
-k = int(input())
+k = input()
 arr = k.split()
 
 a, b = int(arr[0]), int(arr[1])
 
-print(a>=b)
-print(a>b)
-print(a<=b)
-print(a<b)
+print(int(a>=b))
+print(int(a>b))
+print(int(a<=b))
+print(int(a<b))
