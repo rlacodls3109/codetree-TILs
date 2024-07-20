81509ff158224b37f939b2ee87fd6a121b5b68b3
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-20 14:41:54 +0900

    Title: 알파벳에 따른 평가, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,19 @@
+k = input()
+
+if k == 'S':
+    print("Superior")
+
+elif k == 'A':
+    print("Excellent")
+
+elif k == 'B':
+    print("Good")
+
+elif k == 'C':
+    print("Usually")
+
+elif k == 'D':
+    print("Effort")
+
+else:
+    print("Failure")
