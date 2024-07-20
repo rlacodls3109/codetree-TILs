47125e62f5cd1a2016df5dc6820cedfb6ee12810
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-20 22:37:09 +0900

    Title: 1까지 나누기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240720/1까지 나누기/divide-by-1.py
+++ 240720/1까지 나누기/divide-by-1.py
@@ -1,12 +1,10 @@
 n = int(input())
 i = 0
-cnt = 0
 while(1):
-    cnt += 1
     i += 1
 
-    if (n / i) < 1:
+    if (n / i) <= 1:
         break
     else :
         n = n // i
-print(cnt)
+print(i)
