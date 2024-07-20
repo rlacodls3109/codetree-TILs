289102bad947c620551af5fecc98e576925a9dc5
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-20 17:50:18 +0900

    Title: 일 수 구하기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/일 수 구하기/number-of-days-in-month.py
@@ -0,0 +1,17 @@
+m = int(input())
+
+if m == 2:
+    print(28)
+
+else : 
+    if m <= 7 :
+        if m % 2 != 0 :
+            print(31)
+        else :
+            print(30)
+    
+    else :
+        if m % 2 != 0:
+            print(30)
+        else:
+            print(31)
