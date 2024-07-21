451432ee349c877a702d5beaaa2ce48e87e46db0
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-21 16:36:40 +0900

    Title: 별표 출력하기 11, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240721/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(2*n+1):
+    for j in range(2*n+1):
+        if i % 2 == 0:
+            print("*",end=" ")
+        elif j % 2 == 0 :
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
