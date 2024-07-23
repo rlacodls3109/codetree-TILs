41591c79f5c26a05dbca3fba51871b724cc4b9e6
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 00:26:12 +0900

    Title: 나눗셈의 나머지, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240724/나눗셈의 나머지/remainder-of-division.py
+++ 240724/나눗셈의 나머지/remainder-of-division.py
@@ -4,13 +4,13 @@ arr = list(map(int,input().split()))
 a,b = arr[0],arr[1]
 
 while(1):
-    a = a/b
+    a = a//b
     rest[int(a%b)] += 1
     if a <= 1:
         break
 
 sum = 0
-
+print(rest)
 for i in rest:
     sum += i*i
 
