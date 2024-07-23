3d7bf7e057a26b017738d10dcbcb27e6b6d03c3c
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 01:43:23 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240724/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,21 @@
+arr = list(map(int,input().split()))
+n1, n2 = arr[0], arr[1]
+
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+for i in range(len(A)):
+    if A[i] == B[0]:
+        for j in range(len(B)):
+            if A[i+j] != B[j]:
+                result = 'No'
+                break
+            else:
+                result = 'Yes'
+        if result == 'Yes':
+            break
+
+    else : 
+        result = 'No'
+    
+print(result)
