d84b2f33375fccbd17ebe6f61d9a1e5c57713034
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 22:11:21 +0900

    Title: 두 개의 격자 비교하기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240724/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,27 @@
+n, m = tuple(map(int,input().split()))
+
+arr_2d_1 = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+arr_2d_2 = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+arr_2d_3 = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+for i in range(n):
+    for j in range(m):
+        if arr_2d_1[i][j] == arr_2d_2[i][j]:
+            arr_2d_3[i][j] = 0
+        else:
+            arr_2d_3[i][j] = 1
+
+for arr in arr_2d_3:
+    for elem in arr:
+        print(elem,end=" ")
+    print()
