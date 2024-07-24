a2b1ae74312a6d192b55eb712638cc4b0dd85146
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 21:54:43 +0900

    Title: 숫자 직사각형, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/숫자 직사각형/rectangle-of-numbers.py
+++ 240724/숫자 직사각형/rectangle-of-numbers.py
@@ -1,6 +1,6 @@
 arr = list(map(int,input().split()))
 n, m = arr[0], arr[1]
-
+num = 1
 arr_2d = [
     [0 for _ in range(m)]
     for _ in range(n)
@@ -8,7 +8,8 @@ arr_2d = [
 
 for i in range(n):
     for j in range(m):
-        arr_2d[i][j] += 1
+        arr_2d[i][j] += num
+        num += 1
 
 for array in arr_2d:
     for elem in array:
