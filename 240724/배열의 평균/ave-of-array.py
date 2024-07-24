54c43f8a5b7c8c4f75b6387b8adfe9e66a259d92
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 21:45:27 +0900

    Title: 배열의 평균, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240724/배열의 평균/ave-of-array.py
@@ -0,0 +1,28 @@
+arr [
+    list(map(int,input().split()))
+    for _ in range(2)
+]
+
+for i in range(2):
+    sum = 0
+    for j in range(4):
+        sum += arr[i][j]
+        avg = sum/4
+        print(f"{avg:.1f}",end =" ")
+print()
+
+for j in range(4):
+    sum = 0
+    for i in range(2):
+        sum += arr[i][j]
+        avg = sum / 2
+        print(f"{avg:.1f}",end=" ")
+print()
+
+sum = 0
+for i in range(2):
+    for j in range(4):
+        sum += arr[i][j]
+
+avg = sum / 8
+print(f"{avg:.1f}")
