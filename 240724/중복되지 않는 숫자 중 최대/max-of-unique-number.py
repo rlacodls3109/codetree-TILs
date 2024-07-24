64d1d09125085b66aba70de53c5d54c7ed51ebe5
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 10:15:27 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240724/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -2,12 +2,18 @@ n = int(input())
 arr = list(map(int,input().split()))
 tf = 'True'
 max = -1
+check = []
 for i in range(0,len(arr)) :
     tf = 'True'
+    if arr[i] in check:
+        tf= 'False'
+        continue
     if i+1 <= len(arr):
         for j in range(i+1,len(arr)) :
             if arr[i] == arr[j]:
                 tf = 'False'
+                check.append(arr[j])
+
         if tf == 'True' and arr[i] > max:
             max = arr[i]
 
