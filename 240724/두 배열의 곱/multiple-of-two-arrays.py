028b72a0a2f574e97f160431708cf5ada66ce7ea
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 22:04:50 +0900

    Title: 두 배열의 곱, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240724/두 배열의 곱/multiple-of-two-arrays.py
+++ 240724/두 배열의 곱/multiple-of-two-arrays.py
@@ -2,6 +2,7 @@ arr_2d_1 = [
     list(map(int,input().split()))
     for _ in range(3)
 ]
+a = input()
 arr_2d_2 = [
     list(map(int,input().split()))
     for _ in range(3)
@@ -11,5 +12,5 @@ arr_2d_2 = [
 
 for i in range(3):
     for j in range(3):
-        print(arr_2d_1[i][j]*arr_2d_2[i][j],end=" ")
+        print(arr_2d_1[i][j]* arr_2d_2[i][j],end=" ")
     print()
