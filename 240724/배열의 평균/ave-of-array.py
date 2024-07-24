edec307555aa5e5f2a159d0542552fe7eb8c6a51
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-24 21:47:27 +0900

    Title: 배열의 평균, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/배열의 평균/ave-of-array.py
+++ 240724/배열의 평균/ave-of-array.py
@@ -1,4 +1,4 @@
-arr [
+arr=[
     list(map(int,input().split()))
     for _ in range(2)
 ]
@@ -7,16 +7,16 @@ for i in range(2):
     sum = 0
     for j in range(4):
         sum += arr[i][j]
-        avg = sum/4
-        print(f"{avg:.1f}",end =" ")
+    avg = sum/4
+    print(f"{avg:.1f}",end =" ")
 print()
 
 for j in range(4):
     sum = 0
     for i in range(2):
         sum += arr[i][j]
-        avg = sum / 2
-        print(f"{avg:.1f}",end=" ")
+    avg = sum / 2
+    print(f"{avg:.1f}",end=" ")
 print()
 
 sum = 0
