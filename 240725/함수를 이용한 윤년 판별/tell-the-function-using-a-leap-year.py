e222c0c0c3e33c650f25f90060775b2d694f3d0d
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-25 15:46:34 +0900

    Title: 함수를 이용한 윤년 판별, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,11 @@
+def is_yoon(y):
+    if y % 4 == 0:
+        if y % 100 == 0:
+            if y % 400 != 0:
+                return "true"
+            return "false"
+        return "true"
+
+y = int(input())
+
+print(is_yoon(y))
