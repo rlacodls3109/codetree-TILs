46641dc39f05b78df58a6c415efe5a7b4d279a9d
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-25 15:47:56 +0900

    Title: 함수를 이용한 윤년 판별, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240725/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -2,8 +2,7 @@ def is_yoon(y):
     if y % 4 == 0:
         if y % 100 == 0:
             if y % 400 != 0:
-                return "true"
-            return "false"
+                return "false"
         return "true"
     return "false"
 
