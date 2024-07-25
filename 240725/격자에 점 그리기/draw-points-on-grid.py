3ff8a8c20ac6b39849222f9bd6518bbd0376c84a
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-25 11:38:29 +0900

    Title: 격자에 점 그리기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240725/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,15 @@
+n, m = tuple(map(int,input().split()))
+
+result = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(m):
+    r, c = tuple(map(int,input().split()))
+    result[r-1][c-1] = m+1
+
+for arr in result:
+    for elem in arr:
+        print(elem,end=" ")
+    print()
