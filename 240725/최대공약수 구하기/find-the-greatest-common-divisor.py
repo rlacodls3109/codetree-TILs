d566e32fc7eb0b12a6595433a02199869625ab51
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-25 13:05:38 +0900

    Title: 최대공약수 구하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240725/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,7 +1,9 @@
 n, m = tuple(map(int,input().split()))
 
-def 
-for i in range(n,0,-1):
-    if n % i == 0 and m % i == 0:
-        print(i)
-        break
+def find(n,m):
+    for i in range(n,0,-1):
+        if n % i == 0 and m % i == 0:
+            print(i)
+            break
+
+find(n,m)
