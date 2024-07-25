8c11811f454f3af252ba66ebb65b3aa8eedcb138
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-25 11:48:06 +0900

    Title: 숫자로 이루어진 사각형, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240725/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,12 @@
+def draw(n):
+    cnt = 1
+    for i in range(n):
+        for j in range(n):
+            print(cnt,end=" ")
+            cnt += 1
+            if cnt == 10:
+                cnt = 1
+        print()
+            
+n = int(input())
+draw(n)
