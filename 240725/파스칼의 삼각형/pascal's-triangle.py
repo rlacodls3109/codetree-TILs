82a07e6873f12fa385aeab4662639806198064b1
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-25 11:12:25 +0900

    Title: 파스칼의 삼각형, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240725/파스칼의 삼각형/pascal's-triangle.py
+++ 240725/파스칼의 삼각형/pascal's-triangle.py
@@ -9,7 +9,7 @@ for i in range(n):
     triangle[i][0] = 1
     triangle[i][i] = 1
 
-for i in range(2,n):
+for i in range(2,n): # for i in range(n)으로 해도 됨. i=0, i=1일때에는 실행 안됨
     for j in range(1,i):
         triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
 
