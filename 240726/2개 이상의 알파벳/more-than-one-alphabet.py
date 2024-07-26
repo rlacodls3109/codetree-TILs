c407d24baac4242270ae14d2e9d700dda69f4903
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-26 15:27:43 +0900

    Title: 2개 이상의 알파벳, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240726/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -8,6 +8,16 @@ def answer(string):
             return True
     return False
 
+'''
+# 코드 간소화
+# 서로다른 알파벳이 한번이라도 나오면 true를 리턴한다.
+# 즉, 문자열의 맨 첫 문자와 나머지 문자를 비교해서 다른게 나오면 바로 종료하면 된다.
+
+for i in range(1,len(string)):
+    if string[i] != string[0]:
+        return True
+    return False
+'''
 
 A = input()
 if answer(A):
