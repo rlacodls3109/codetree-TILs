a5ba49bab0548089f59b1d309778929bae6fec09
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-26 15:22:18 +0900

    Title: 2개 이상의 알파벳, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240726/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -2,7 +2,7 @@ def answer(string):
     cnt = 0
     for i in range(len(string)-1):
         for j in range(i+1,len(string)):
-            if string[i] == string[j]:
+            if string[i] != string[j]:
                 cnt += 1
         if cnt >= 2:
             return True
