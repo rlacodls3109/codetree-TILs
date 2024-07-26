6425308464eaa18a08623e30ff49e9121c2edf1c
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-26 15:21:38 +0900

    Title: 2개 이상의 알파벳, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,16 @@
+def answer(string):
+    cnt = 0
+    for i in range(len(string)-1):
+        for j in range(i+1,len(string)):
+            if string[i] == string[j]:
+                cnt += 1
+        if cnt >= 2:
+            return True
+    return False
+
+
+A = input()
+if answer(A):
+    print("Yes")
+else:
+    print("No")
