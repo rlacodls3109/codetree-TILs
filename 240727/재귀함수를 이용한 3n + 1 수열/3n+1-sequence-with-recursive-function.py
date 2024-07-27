7ce0d91367291832df7f536ae09a94eb12f0d4e1
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-27 20:44:29 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
+++ 240727/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.py
@@ -1,3 +1,5 @@
+'''
+# 내정답 : a라느 count 변수를 안써도 return 값에 1을 하나씩 더하면 되는군!,,
 a = 0 
 
 def answer(n):
@@ -10,7 +12,19 @@ def answer(n):
     else :
         answer((n*3)+1)
 
-
 n = int(input())
 answer(n)
 print(a)
+'''
+
+def count_number(a):
+    if a == 1:
+        return 0
+
+    if a % 2 == 0:
+        return count_number(a // 2) + 1
+    else:
+        return count_number(3 * a + 1) + 1
+
+n = int(input())
+print(count_number(n))
