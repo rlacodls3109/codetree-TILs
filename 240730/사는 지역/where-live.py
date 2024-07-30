6bd8875b0372ae01eb276069800387d05b1b6a1e
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-30 15:53:45 +0900

    Title: 사는 지역, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/사는 지역/where-live.py
+++ 240730/사는 지역/where-live.py
@@ -11,14 +11,20 @@ for _ in range(n):
     name, code, country = tuple(input().split())
     peoples.append(People(name,code,country))
 
-max = 0
 idx = 0
-for i in range(n):
-    char = peoples[i].name[0]
-    if max < int(ord(char)):
-        max = int(ord(char))
-        idx = i
 
+name_list = [
+    peoples[i].name
+    for i in range(n)
+]
+
+name_list.sort()
+
+for i in range(n):
+    if name_list[-1] == peoples[i].name:
+        idx = i
+        break
+    
 people1 = peoples[idx]
 print("name",people1.name)
 print("addr",people1.code)
