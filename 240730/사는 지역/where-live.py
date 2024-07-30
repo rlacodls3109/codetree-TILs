2ea3c7dbbb7891b086591213fd634cd0af310be7
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-07-30 15:55:11 +0900

    Title: 사는 지역, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/사는 지역/where-live.py
+++ 240730/사는 지역/where-live.py
@@ -29,3 +29,31 @@ people1 = peoples[idx]
 print("name",people1.name)
 print("addr",people1.code)
 print("city",people1.country)
+
+
+
+'''
+이런 방법도 되는군..
+class Address:
+    def __init__(self, name, address, region):
+        self.name = name
+        self.address = address
+        self.region = region
+        
+        
+# 변수 선언 및 입력
+n = int(input())
+arr = [tuple(input().split()) for _ in range(n)]
+people = [Address(name, address, region) for name, address, region in arr]
+
+# 사전순으로 이름이 가장 느린 사람 찾기
+target_idx = 0
+for i, person in enumerate(people):
+    if person.name > people[target_idx].name:
+        target_idx = i
+
+# 결과 출력
+print(f"name {people[target_idx].name}")
+print(f"addr {people[target_idx].address}")
+print(f"city {people[target_idx].region}")
+'''
