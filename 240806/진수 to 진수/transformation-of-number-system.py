421018da371686d6b5bbe81607dcabffaea61e44
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-06 16:26:15 +0900

    Title: 진수 to 진수, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/진수 to 진수/transformation-of-number-system.py
+++ 240806/진수 to 진수/transformation-of-number-system.py
@@ -2,13 +2,12 @@
 def a_to_ten(n,a):
     #주어진 a진수 n의 자리수를 구한다.
     n_length = len(n)
-    
     ten = 0 # 변환된 10진수를 저장할 변수
 
-        #n의 자릿수만큼 반복한다.
-        for i in range(n_length):
-            # 10진수 변환 방법 : 각각의 자리 숫자에 a의 자릿수(0부터 시작)제곱을 곱해서 결과에 더해준다.
-            ten += (int(n[i]) * (a**(n_length-i-1)))
+    #n의 자릿수만큼 반복한다.
+    for i in range(n_length):
+        # 10진수 변환 방법 : 각각의 자리 숫자에 a의 자릿수(0부터 시작)제곱을 곱해서 결과에 더해준다.
+        ten += (int(n[i]) * (a**(n_length-i-1)))
     return ten
 
 #10진수를 b진수로 바꾸는 함수
