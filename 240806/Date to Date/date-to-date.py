669b285c3d639dfa981bc20fe303ff73a4b833c8
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-06 12:16:07 +0900

    Title: Date to Date, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/Date to Date/date-to-date.py
@@ -0,0 +1,26 @@
+m1, d1, m2, d2 = tuple(map(int,input().split()))
+
+# 각 날마다 달이 얼마나 있는지
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+#1월 1일부터 며칠이 지났는지 계산하기 위한 변수 
+start_days = 0
+end_days = 0
+
+#시작일부터 며칠인지 계산
+for i in range(1,m1):
+    start_days += num_of_days[i]
+
+start_days += d1
+
+#마지막날부터 며칠인지 계산
+for j in range(1,m2):
+    end_days += num_of_days[j]
+
+end_days += d2
+
+#흐른 날짜 계산
+answer = end_days - start_days + 1
+
+
+print(answer)
