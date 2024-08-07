a6fae27cef2911def8a1145b0f87527917d9e254
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-07 16:35:10 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240807/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -5,9 +5,12 @@ arr = list(map(int,input().split()))
 cnt = 0
 max_cnt = 0
 
+
 for i in range(n):
+    # 현재 원소가 t보다 크다면 카운트
     if arr[i] > t :
         cnt += 1
+    # 작다면 cnt 초기화
     else: 
         cnt = 0
     
