999f157c1364d429eea89fe3ee0cc5d76e3240e6
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-07 16:28:24 +0900

    Title: 연속되는 수 4, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/연속되는 수 4/continuous-number4.py
+++ 240807/연속되는 수 4/continuous-number4.py
@@ -1,5 +1,6 @@
 N = int(input())
 
+#숫자 입력받아 배열로 저장
 arr = [
     int(input())
     for _ in range(N)
@@ -8,12 +9,18 @@ arr = [
 cnt = 0
 max_cnt = 0
 
+
 for i in range(N):
+    # 맨 처음 원소는 비교대상 없으므로 카운트
+    # 직전 원소보다 크다면 카운트
     if i == 0 or arr[i] > arr[i-1]:
         cnt += 1
+    
+    # 아니면 카운트 1부터 다시 시작
     else:
         cnt = 1
     
+    # 매 반복마다 max값 갱신
     max_cnt = max(cnt,max_cnt)
 
 print(max_cnt)
