02f7218aa75b0e18534f279172b71ddb13ed434e
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-08 13:49:51 +0900

    Title: 벌금은 누구에게, Time: 148ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240808/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,16 @@
+N, M, K = tuple(map(int,input().split()))
+
+check_student = [0] * (N+1)
+student_payturn = [0] * (N+1)
+
+pay_turn = 1
+
+for _ in range(M):
+    student_num = int(input())
+    check_student[student_num] += 1
+
+    if check_student[student_num] == K:
+        student_payturn[pay_turn] = student_num
+        pay_turn += 1
+
+print(student_payturn[1])
