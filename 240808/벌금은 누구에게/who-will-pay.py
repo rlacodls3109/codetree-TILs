70853b7c0557c308b509714e882ac2955eb6e46c
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-08 14:12:41 +0900

    Title: 벌금은 누구에게, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/벌금은 누구에게/who-will-pay.py
+++ 240808/벌금은 누구에게/who-will-pay.py
@@ -1,16 +1,22 @@
 N, M, K = tuple(map(int,input().split()))
 
+# 학생의 벌칙 횟수 셀 배열
 check_student = [0] * (N+1)
-student_payturn = [-1] * (N+1)
 
-pay_turn = 1
+# 벌금 내는 순서대로 번호를 넣기 위한 배열
+student_payturn = []
+
 
 for _ in range(M):
     student_num = int(input())
+    #벌칙 횟수를 배열에 저장
     check_student[student_num] += 1
 
+    # K번 이상 벌칙을 받은 사람 번호가 student_payturn 배열에 저장된다.
     if check_student[student_num] == K:
-        student_payturn[pay_turn] = student_num
-        pay_turn += 1
+        student_payturn.append(student_num)
 
-print(student_payturn[1])
+# 벌금 내는 순서의 배열이 비어있으면 -1, 아니라면 맨 첫번째 사람의 번호를 출력한다.
+ans = -1 if len(student_payturn) == 0 else student_payturn[0]  
+
+print(ans)
