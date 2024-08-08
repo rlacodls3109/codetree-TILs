274ebcc26120635b323a58a1f9832b0128666846
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-08 15:45:30 +0900

    Title: 좌우로 움직이는 로봇, Time: 242ms, Memory: 41MB, Status: Passed - CodeTree

--- 240808/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240808/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -2,9 +2,11 @@ OFFSET = 1000000
 
 n, m = tuple(map(int,input().split()))
 
+# 매 초마다 어느 위치에 있는 지 기록하는 배열
 A = [0] * (OFFSET+1)
 B = [0] * (OFFSET+1)
 
+#a의 이동경로 기록
 a_time = 1
 for _ in range(n):
     t,d = tuple(input().split())
@@ -14,7 +16,7 @@ for _ in range(n):
         A[a_time] = A[a_time-1] + (1 if d =='R' else -1)
         a_time += 1
 
-
+#b의 이동경로 기록
 b_time = 1
 for _ in range(m):
     t,d = tuple(input().split())
@@ -24,20 +26,20 @@ for _ in range(m):
         B[b_time] = B[b_time-1] +(1 if d =='R' else -1)
         b_time += 1
 
+# 먼저 이동을 끝낸 사람의 위치배열 값을 이동중인 사람의 시간에 맞추어 마지막지점을 넣어주어야 한다.
 if a_time < b_time:
     while(a_time != b_time):
         A[a_time] = A[a_time-1]
         a_time += 1
-
 elif a_time > b_time:
     while(a_time != b_time):
         B[b_time] = B[b_time-1]
         b_time += 1
 
-
+# 만나는 횟수를 구한다.
 cnt = 0
-
 for time in range(1,a_time):
+    # 직전에 같은 위치가 아니면서 현재에 같은 위치에 서있을 때 카운트
     if A[time] == B[time] and A[time-1] != B[time-1]:
         cnt += 1
 
