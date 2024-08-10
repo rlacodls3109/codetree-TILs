2573ea11806012a7c11bf87e1964ff8d4b14f95c
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-10 11:51:12 +0900

    Title: 1이 3개 이상 있는 위치, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240810/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+arr = []
+
+#dxs는 행의 이동, dys는 열의 이동
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+#인덱스가 행렬의 범위 내에 있는 지 확인하는 함수
+def in_range(row,col):
+    return row < n and col < n
+
+for _ in range(n):
+        arr.append(list(map(int,input().split())))
+
+ans = 0
+
+#주어진 행렬 순회
+for row in range(n): #행
+    for col in range(n): #열
+        cnt = 0 #각 요소마다 cnt 초기화
+        for dx,dy in zip(dxs,dys):
+            n_row, n_col = row+dx, col+dy #각 요소의 상하좌우 위치
+            if in_range(n_row,n_col) and arr[n_row][n_col] == 1:
+                cnt += 1
+        
+        if cnt >= 3:
+            ans += 1
+
+print(ans)
