7ba15bda9d0952400d0ac5a9cc8f41989b3aa9da
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-12 23:51:19 +0900

    Title: 최고의 13위치, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/최고의 13위치/best-place-of-13.py
+++ 240812/최고의 13위치/best-place-of-13.py
@@ -1,5 +1,6 @@
 n = int(input())
 
+# 주어진 동전 정보
 coin = [
     list(map(int,input().split()))
     for _ in range(n)
@@ -8,6 +9,7 @@ coin = [
 max_cnt = 0
 for i in range(n):
     coin_cnt = 0
+    # 격자를 벗어나는 경우는 제외해야 함에 유의 가로 3의 길이이므로 최대 시작 인덱스가 n-3여야 함
     for j in range(n-2):
         coin_cnt = coin[i][j] + coin[i][j+1] + coin[i][j+2]
         max_cnt = max(max_cnt,coin_cnt)
