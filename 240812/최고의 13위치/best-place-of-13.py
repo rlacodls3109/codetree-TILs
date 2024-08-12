3621b496e4dac415dd684c44c1046d7435b1a9ce
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-12 23:49:37 +0900

    Title: 최고의 13위치, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240812/최고의 13위치/best-place-of-13.py
+++ 240812/최고의 13위치/best-place-of-13.py
@@ -10,7 +10,6 @@ for i in range(n):
     coin_cnt = 0
     for j in range(n-2):
         coin_cnt = coin[i][j] + coin[i][j+1] + coin[i][j+2]
-    
-    max_cnt = max(max_cnt,coin_cnt)
+        max_cnt = max(max_cnt,coin_cnt)
 
 print(max_cnt)
