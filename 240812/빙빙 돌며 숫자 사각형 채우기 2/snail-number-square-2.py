5aeb298521635811f1b9d22365439c183987d837
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-12 17:40:01 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240812/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 240812/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -1,25 +1,34 @@
 n, m = tuple(map(int,input().split()))
 
+# D, R, U, L 순서
 mov_x = [1,0,-1,0]
 mov_y = [0,1,0,-1]
 n_dir = 0
+
 def in_range(r,c):
     return 0 <= r and r < n and 0 <= c and c < m
 
+# 정답 배열 생성
 arr = [
     [0] * m
     for _ in range(n)
 ]
 
+# 맨 처음 위치
 x,y = 0,0
 for i in range(1,n*m+1):
-    arr[x][y] = i
+    # 현 위치에 값을 채움
+    arr[x][y] = i 
+    # 현재 방향을 기준으로 그 다음 위치 값 계산
     nx = x + mov_x[n_dir]
     ny = y + mov_y[n_dir]
 
+    # 다음 위치가 배열의 범위를 벗어나거나, 값이 채워져 있을 경우,
+    # 방향을 시계반대방향으로 전환
     if not in_range(nx,ny) or arr[nx][ny] != 0:
         n_dir = (n_dir + 1) % 4
     
+    # x,y값 갱신
     x += mov_x[n_dir]
     y += mov_y[n_dir]
 
