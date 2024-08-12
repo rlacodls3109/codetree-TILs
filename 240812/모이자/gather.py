a65c5e8e02520f2a05acacd5609af581c9f09ff6
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-12 23:27:03 +0900

    Title: 모이자, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240812/모이자/gather.py
+++ 240812/모이자/gather.py
@@ -1,5 +1,6 @@
 import sys
 
+# 최솟값을 시스템상 최댓값으로 설정
 INT_MIN = sys.maxsize
 
 n = int(input())
@@ -7,11 +8,15 @@ n = int(input())
 people = list(map(int,input().split()))
 
 dis_min = INT_MIN
+
+#pos 번 집으로 모였을 때의 합을 구함
 for pos in range(n):
     total_dis = 0
     for each in range(n):
         total_dis += people[each] * abs(pos - each)
-    if dis_min > total_dis:
-        dis_min = total_dis
+    
+    # 가능한 거리의 합 중 최소값을 구함
+    dis_min = min(dis_min,total_dis)
+
 
 print(dis_min)
