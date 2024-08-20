6e9663b26472006ac137a9e0bde3708a5b9eec20
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-20 15:25:01 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240820/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240820/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,8 +1,11 @@
 given = input()
 
 cnt = 0
+#연속하는 두 괄호가 모두 '('라면
+#반복횟수는 len(given) - 1 이어야 한다.
 for i in range(len(given)-1):
     if given[i] == '(' and given[i+1] =='(':
+        #그 이후의 문자열 중 연속하는 ')' 가 있는지 찾아본다
         for j in range(i+2,len(given)-1):
             if given[j] == ')' and given[j+1] == ')':
                 cnt += 1
