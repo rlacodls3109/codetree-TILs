93244a2a6943d2b5c925caf2dd0629813be598c8
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-27 21:58:15 +0900

    Title: 구간 중 최대 합, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240827/구간 중 최대 합/max-sum-of-subarray.py
+++ 240827/구간 중 최대 합/max-sum-of-subarray.py
@@ -4,12 +4,14 @@ num = list(map(int,input().split()))
 
 result = -1
 
-# 
+# 구간의 시작점 잡기
 for i in range(n-k+1):
     sum_num = 0
+    # 해당 구간 내의 원소의 합을 구한다.
     for j in range(i,i+k):
         sum_num += num[j]
     
+    # 최댓값 구하기
     result = max(sum_num,result)
 
 print(result)
