3360ba1dcf87db5f2c760374e4859ff7b479b275
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-27 11:42:40 +0900

    Title: 오목, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/오목/O-mok.py
+++ 240827/오목/O-mok.py
@@ -15,10 +15,18 @@ def check_Rdiagonal(x,y,color):
 def check_Ldiagonal(x,y,color):
     return arr[x-1][y+1] == color and arr[x-2][y+2] == color and arr[x+1][y-1] == color and arr[x+2][y-2] == color
 
+winner = 0
+
 for i in range(2,len(arr)-1):
     for j in range(2,len(arr[1])-1):
         if arr[i][j] != 0:
             color = arr[i][j]
             if check_side(i,j,color) or check_updown(i,j,color) or check_Rdiagonal(i,j,color) or check_Ldiagonal(i,j,color):
-                print(color)
-                print(i+1,j+1)
+                winner = color
+                winner_pos_x, winner_pos_y = i+1, j+1
+
+if winner == 0:
+    print(winner)
+else :
+    print(winner)
+    print(winner_pos_x,winner_pos_y)
