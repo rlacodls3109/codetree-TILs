0b87929a111ed4ced3c532f4a558624d738a416d
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-27 11:35:18 +0900

    Title: 오목, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/오목/O-mok.py
+++ 240827/오목/O-mok.py
@@ -9,9 +9,12 @@ def check_updown(x,y,color):
 def check_side(x,y,color):
     return arr[x][y-1] == color and arr[x][y-2] == color and arr[x][y+1] == color and arr[x][y+2] == color
 
+def check_diagonal(x,y,color):
+    return arr[x-1][y-1] == color and arr[x-2][y-2] == color and arr[x+1][y+1] == color and arr[x+2][y+2] == color
 for i in range(2,len(arr)-1):
     for j in range(2,len(arr[1])-1):
         if arr[i][j] != 0:
-            if check_side(i,j,arr[i][j]) or check_updown(i,j,arr[i][j]):
-                print(arr[i][j])
+            color = arr[i][j]
+            if check_side(i,j,color) or check_updown(i,j,color) or check_diagonal(i,j,color):
+                print(color)
                 print(i+1,j+1)
