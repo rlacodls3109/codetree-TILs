c15154d8322b0cb97b4718efb229bcd4fde74456
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-27 21:42:43 +0900

    Title: 특정 수와 근접한 합, Time: 105ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240827/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,15 @@
+import sys
+n, s = tuple(map(int,input().split()))
+number_list = list(map(int,input().split()))
+result = sys.maxsize
+
+number_total = 0
+for num in number_list:
+    number_total += num
+
+for i in range(n):
+    for j in range(i,n):
+        cal = number_total - number_list[i] - number_list[j]
+        result = min(result,abs(s - cal))
+
+print(result)
