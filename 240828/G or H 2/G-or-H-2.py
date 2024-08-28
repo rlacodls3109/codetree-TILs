40b089e35b2182ded7f8ad143bda1ba9be5664bd
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-28 14:02:28 +0900

    Title: G or H 2, Time: 228ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/G or H 2/G-or-H-2.py
+++ 240828/G or H 2/G-or-H-2.py
@@ -15,11 +15,11 @@ result = -1
 # p = 탐색구간의 길이 즉, 사진의 크기
 for p in range(len(arr)):
     # i = 탐색 구간의 시작 위치
-    for i in range(len(arr)-p-1):
+    for i in range(len(arr)-p):
         cntG, cntH = 0,0
         pic_length = 0
 
-        # 구간의 시작점과 끝점에 알파벳이 없으면 계산하지 않는다.
+        # 구간의 시작점이나 끝점에 알파벳이 없으면 계산하지 않는다.
         if arr[i] == 0 or arr[i+p] == 0:
             continue
 
@@ -31,7 +31,7 @@ for p in range(len(arr)):
                 cntG += 1
             elif arr[j] == 'H':
                 cntH += 1
-            
+
         # 오로지 G로만 혹은 H로만 이루어져 있거나, G와 H가 정확히 같은 개수만큼 나오게 사진을 찍고싶을 때,
         if cntG == cntH or cntG == 0 or cntH == 0:
             # 배열의 길이 저장
