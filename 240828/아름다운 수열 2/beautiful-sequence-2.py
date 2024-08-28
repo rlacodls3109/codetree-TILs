6f73c070e52f91b47d2b67275953a6fc206b9a94
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-28 11:02:19 +0900

    Title: 아름다운 수열 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/아름다운 수열 2/beautiful-sequence-2.py
+++ 240828/아름다운 수열 2/beautiful-sequence-2.py
@@ -4,13 +4,27 @@ B = list(map(int,input().split()))
 
 result = 0
 
+# i = 구간의 시작점
 for i in range(n-m+1):
+    # 아름다운 수열이 존재하는 지 확인하기 위한 bool 함수
     check_in_B = True
+
+    # 탐색할 때 사용할 새로운 리스트를 만든다.
+    new_B = [
+            B[i]
+            for i in range(len(B))
+        ]
+    
+    # 수열 A에 대해 구간탐색
     for j in range(i,i+m):
-        if A[j] not in B:
+        
+        # B에 있는 값이 아니라면 즉시 탐색을 종료한다.
+        if A[j] not in new_B:
             check_in_B = False
             break
-    
+        # B에 있는 값이라면 new_B에서 요소를 제거한다
+        new_B.remove(A[j])
+
     if check_in_B :
         result += 1
 
