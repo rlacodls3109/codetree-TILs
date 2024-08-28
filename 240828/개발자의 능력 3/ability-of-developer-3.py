b45ec2ad52645802c8fff6b0a80153e598c4d2e0
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-28 15:09:36 +0900

    Title: 개발자의 능력 3, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240828/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,19 @@
+import sys
+
+arr = list(map(int,input().split()))
+
+#세 숫자로 이루어진 두 팀의 차를 계산하는 함수
+def get_diff(a,b,c):
+    sum1 = arr[a] + arr[b] + arr[c]
+    sum2 = sum(arr) - sum1
+    return abs(sum1-sum2)
+
+result = sys.maxsize
+
+
+for i in range(6):
+    for j in range(i,6):
+        for k in range(j,6):
+            result = min(result,get_diff(i,j,k))
+
+print(result)
