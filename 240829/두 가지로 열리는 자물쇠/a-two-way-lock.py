4b520b3e981660a2a55e3768c58a737969492b05
rlacodls3109 <56792033+rlacodls3109@users.noreply.github.com>
2024-08-29 11:21:51 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240829/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240829/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -20,11 +20,11 @@ for i in range(1,n+1):
     for j in range(1,n+1):
         for k in range(1,n+1):
             # 모든 자리에 대해 첫 번째 조합과 거리가 2이내라면
-            if diff_is_two(i,a1) and diff_is_two(j,b1) and diff_is_two(k,c1):
+            if dist_is_two(i,a1) and dist_is_two(j,b1) and dist_is_two(k,c1):
                 result += 1
             # 모든 자리에 대해 두 번째 조합과 거리가 2 이내라면
             # 둘 다 만족하는 경우는 하나로 생각하므로 elif
-            elif diff_is_two(i,a2) and diff_is_two(j,b2) and diff_is_two(k,c2):
+            elif dist_is_two(i,a2) and dist_is_two(j,b2) and dist_is_two(k,c2):
                 result += 1
 
 print(result)
